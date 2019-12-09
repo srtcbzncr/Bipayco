8a718f4c0349eedddd42f11e5dd51a68a6673ed2
Sertaç Bazancir <bzncrsrtc@gmail.com>
2019-12-09 17:13:19 +0300

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,34 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js"></script>
+    <script src="{{asset('js/simplebar.js')}}"></script>
+    <script src="{{asset('js/dropzone.min.js')}}"></script>
+    <script src="{{asset('js/uikit.js')}}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{asset('css/uikit.css')}}" rel="stylesheet">
+    <link href="{{asset('css/dropzone.css')}}" rel="stylesheet">
+    <link href="{{asset('css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('css/frontawesome.css')}}" rel="stylesheet">
+</head>
+<body>
 <div id="app1">
     <div class="side-nav uk-animation-slide-left-medium" id="side-nav">
         <!--Mobile icon wiill close nav-side   -->
