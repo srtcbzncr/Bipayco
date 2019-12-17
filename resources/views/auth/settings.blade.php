076cef60d26353ecf79a42b0eaa45521ffd90bc5
Bedri Furkan Aydar <bedrifa@outlook.com>
2019-12-17 15:49:51 +0300

    Update settings.blade.php

--- resources/views/auth/settings.blade.php
+++ resources/views/auth/settings.blade.php
@@ -9,31 +9,35 @@
                             <div uk-filter="target: .js-filter" class="uk-margin-medium-top">
                                 <ul class="uk-subnav uk-subnav-pill" style=" border-bottom: 1px solid #e5e5e5" >
                                     <li class="uk-active" uk-filter-control=".tag-personal"  style="border-right: 1px solid #e5e5e5"><a href="#">@lang('front/auth.personal_info')</a></li>
-                                    <li uk-filter-control=".tag-security"  style="border-right: 1px solid #e5e5e5"><a href="#">@lang('front/auth.security')</a></li>
-                                    <li uk-filter-control=".tag-photo"><a href="#">@lang('front/auth.profile_photo')</a></li>
+                                    <li uk-filter-control=".tag-photo" style="border-right: 1px solid #e5e5e5"><a href="#">@lang('front/auth.profile_photo')</a></li>
+                                    <li uk-filter-control=".tag-security" ><a href="#">@lang('front/auth.security')</a></li>
                                 </ul>
                                 <ul class="js-filter" style="list-style-type:none">
                                     <li class="tag-personal">
-                                        <form class="uk-grid-small" uk-grid method="POST" action="{{ route('updatePersonalData') }}">
+                                        <form method="POST" action="{{ route('updatePersonalData') }}">
                                             @csrf
-                                            <div class="uk-width">
-                                                <div class="uk-width-1-2@xl">
-                                                    <div class="uk-form-label"> @lang('front/auth.first_name') </div>
-                                                    <input class="uk-input" name="first_name" type="text" placeholder="@lang('front/auth.first_name')">
+                                            <div class="uk-width-4-1@xl">
+                                                <div class="uk-child-width-1-2 uk-grid">
+                                                    <div>
+                                                        <div class="uk-form-label"> @lang('front/auth.first_name') </div>
+                                                        <input class="uk-input" name="first_name" type="text" placeholder="@lang('front/auth.first_name')">
+                                                    </div>
+                                                    <div>
+                                                        <div class="uk-form-label"> @lang('front/auth.last_name')  </div>
+                                                        <input class="uk-input" name="last_name" type="text" placeholder="@lang('front/auth.last_name')">
+                                                    </div>
                                                 </div>
-                                                <div class="uk-width-1-2@xl">
-                                                    <div class="uk-form-label"> @lang('front/auth.last_name')  </div>
-                                                    <input class="uk-input" name="last_name" type="text" placeholder="@lang('front/auth.last_name')">
+                                                <div class="uk-child-width-2-1">
+                                                    <div>
+                                                        <div class="uk-form-label"> @lang('front/auth.email')  </div>
+                                                        <input class="uk-input" name="email" type="text" placeholder="@lang('front/auth.email')">
+                                                    </div>
+                                                    <div>
+                                                        <div class="uk-form-label"> @lang('front/auth.phone_number')  </div>
+                                                        <input class="uk-input" name="phone_number" type="text" placeholder="@lang('front/auth.phone_number')">
+                                                    </div>
                                                 </div>
-                                                <div class="uk-width-1-2@xl">
-                                                    <div class="uk-form-label"> @lang('front/auth.email')  </div>
-                                                    <input class="uk-input" name="email" type="text" placeholder="@lang('front/auth.email')">
-                                                </div>
-                                                <div class="uk-width-1-2@xl">
-                                                    <div class="uk-form-label"> @lang('front/auth.phone_number')  </div>
-                                                    <input class="uk-input" name="phone_number" type="text" placeholder="@lang('front/auth.phone_number')">
-                                                </div>
-                                                <div class="uk-width-1-2@xl">
+                                                <div class="uk-width-2-1@xl">
                                                     <div class="uk-form-label">@lang('front/auth.city') </div>
                                                     <provinces city-default="@lang('front/auth.province')" district-default="@lang('front/auth.district')"></provinces>
                                                 </div>
@@ -57,16 +61,20 @@
                                     <li class="tag-security">
                                         <form class="uk-grid-small" uk-grid method="POST" action="{{ route('updatePassword') }}">
                                             @csrf
-                                            <div class="uk-width-2-3@xl">
-                                                <div class="uk-width-1-2">
-                                                    <div class="uk-form-label"> @lang('front/auth.password_old') </div>
-                                                    <input class="uk-input" name="old_password" type="password" placeholder="@lang('front/auth.password')">
+                                            <div class="uk-width-2-1@s">
+                                                <div class="uk-child-width-1-2@xl uk-grid">
+                                                    <div class="">
+                                                        <div class="uk-form-label"> @lang('front/auth.password_old') </div>
+                                                        <input class="uk-input" name="old_password" type="password" placeholder="@lang('front/auth.password_old')">
+                                                    </div>
+                                                    <div class="">
+                                                        <div class="uk-form-label"> @lang('front/auth.password_new')  </div>
+                                                        <input class="uk-input" name="new_password" type="password" placeholder="@lang('front/auth.password_new')">
+                                                    </div>
                                                 </div>
-                                                <div class="uk-width-1-2">
-                                                    <div class="uk-form-label"> @lang('front/auth.password_new')</div>
-                                                    <input class="uk-input" name="new_password" type="password" placeholder="@lang('front/auth.password')">
+                                                <div class="uk-width-expand uk-margin-top">
+                                                    <input class="uk-button uk-button-grey button uk-margin" type="submit" value="@lang('front/auth.save')">
                                                 </div>
-                                                <input class="uk-button uk-button-grey button uk-margin" type="submit" value="@lang('front/auth.save')">
                                             </div>
                                         </form>
                                     </li>
@@ -75,7 +83,7 @@
                         </div>
                     </div>
                 </div>
-                <user-status></user-status>
+                <user-status Instructor="true"></user-status>
             </div>
         </div>
     </div>
