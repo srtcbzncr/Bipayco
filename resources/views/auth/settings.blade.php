40afd05dc9fa7c791b89eb8ac41ce607d3360a59
Bedri Furkan Aydar <bedrifa@outlook.com>
2019-12-17 12:10:18 +0300

    settings upload image  updated

--- resources/views/auth/settings.blade.php
+++ resources/views/auth/settings.blade.php
@@ -44,11 +44,11 @@
                                     <div class="uk-width-2-3@m">
                                         <div class="uk-width-1-3@m">
                                             <p>Your profile photo</p>
-                                            <form method="POST" action="{{ route('updateAvatar') }}" >
+                                            <form method="POST" action="{{ route('updateAvatar') }}" enctype="multipart/form-data">
                                                 @csrf
                                                 <div class="fallback">
                                                     <input name="avatar" type="file"/>
-                                                </div>
+                                                </div>-->
                                                 <input class="uk-button uk-button-grey button uk-margin" type="submit" value="Sign in">
                                             </form>
                                             <p>Recomend size 50 x 50</p>
