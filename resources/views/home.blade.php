3cc060db33d55b8fb4c3f10fb42c31c5b996f7ad
Sertaç Bazancir <bzncrsrtc@gmail.com>
2019-12-23 19:41:56 +0300

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div src="/public/images/backgrounds/Homepage-logo.jpg" style="background-color: #3e474f57;background-blend-mode: color-burn;" uk-img class="tm-hero uk-height-large uk-background-norepeat  uk-background-center-center uk-section uk-flex uk-flex-bottom uk-background-cover">
+    <div src="/images/slider_bg.jpg" style="background-image: url(../images/slider_bg.jpg)" uk-img class="tm-hero uk-height-large uk-background-norepeat  uk-background-center-center uk-section uk-flex uk-flex-bottom uk-background-cover">
         <div class="uk-width-1-1 uk-container uk-margin-large-top uk-margin-large-left ">
             <h1 class="uk-text-bold uk-text-white uk-margin-small-bottom"> Learn to code. <br>  One project at a time</h1>
             <h4 class="uk-text-bold uk-light uk-margin-remove-top">Learn how to Build websites apps & games for free   </h4>
@@ -154,7 +154,7 @@
                 <p>@lang('front/auth.popular_in_category')</p>
             </div>
             <div class="uk-float-right">
-                <a href="blog-video-one.html" class="uk-button uk-button-default">See more</a>
+                <a href="blog-video-one.html" class="uk-button uk-button-default">@lang('front/auth.see_more')</a>
             </div>
         </div>
     </div>
