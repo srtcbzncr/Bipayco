622d1ded0b8de77ce30531ebb152fd0b1917ce23
Sertaç Bazancir <bzncrsrtc@gmail.com>
2020-01-03 17:44:13 +0300

    Update CourseController.php

--- app/Http/Controllers/GeneralEducation/CourseController.php
+++ app/Http/Controllers/GeneralEducation/CourseController.php
@@ -29,8 +29,6 @@ class CourseController extends Controller
             'similar_courses' => $similarCourses->getData(),
         ];
 
-        dd($data);
-
         // Response
         if($resp->getResult()){
             return view('general_education.course_detail', $data);
