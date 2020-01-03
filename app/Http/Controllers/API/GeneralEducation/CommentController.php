44387184ecdd857f8beb908031c224ce0f79f12a
Sertaç Bazancir <bzncrsrtc@gmail.com>
2020-01-03 12:47:44 +0300

    Update

--- app/Http/Controllers/API/GeneralEducation/CommentController.php
+++ app/Http/Controllers/API/GeneralEducation/CommentController.php
@@ -3,28 +3,30 @@
 namespace App\Http\Controllers\API\GeneralEducation;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\CommentRequest;
 use App\Repositories\GeneralEducation\CommentRepository;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function create(CommentRequest $request){
-        // Validation
-        $validatedData = $request->validated();
-
+    public function create(Request $request){
         // Repo initialization
         $repo = new CommentRepository;
 
-        // Operations
-        $resp = $repo->create($validatedData);
 
-        // Response
-        if($resp->getResult()){
-            return response()->json(['error' => false, 'message' => __('general_education.comment_is_added')]);
+        if($request->has('content') and $request->has('point')){
+            // Operations
+            $resp = $repo->create($request->toArray());
+
+            // Response
+            if($resp->getResult()){
+                return response()->json(['error' => false, 'message' => __('general_education.comment_is_added')]);
+            }
+            else{
+                return response()->json(['error' => true, 'message' => __('general_education.comment_is_not_added')]);
+            }
         }
         else{
-            return response()->json(['error' => true, 'message' => __('general_education.comment_is_not_added')]);
+            return response()->json(['error' => false, 'message' => 'Lütfen alanları eksiksiz doldurun.']);
         }
     }
 }
