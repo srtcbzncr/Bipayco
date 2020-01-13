06f39c2fd380dd47ae6d51af0f92eb70ac2d4e7f
SergenKoca <sergenkoca@outlook.com.tr>
2020-01-13 12:56:16 +0300

    Update PrepareLessons_SectionFactory.php

--- database/factories/PrepareLessons_SectionFactory.php
+++ database/factories/PrepareLessons_SectionFactory.php
@@ -3,15 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model;
+use App\Models\Curriculum\Subject;
 use App\Models\PrepareLessons\Course;
 use App\Models\PrepareLessons\Section;
 use Faker\Generator as Faker;
 
 $factory->define(Section::class, function (Faker $faker) {
     $course = Course::orderByRaw('RAND()')->take(1)->first();
+    $subject = Subject::orderByRaw('RAND()')->take(1)->first();
     $count = Section::where('course_id', $course->id)->get()->count();
     return [
         'course_id' => $course->id,
+        'subject_id' => $subject->id,
         'no' => $count+1,
         'name' => $faker->sentence(3),
     ];
