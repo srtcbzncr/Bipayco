ba07a92335bf3bf7ee3829af5f4bdd591161a073
SergenKoca <sergenkoca@outlook.com.tr>
2020-01-17 14:17:23 +0300

    Update Course Policy

--- app/Policies/GeneralEducation/CoursePolicy.php
+++ app/Policies/GeneralEducation/CoursePolicy.php
@@ -2,11 +2,13 @@
 
 namespace App\Policies\GeneralEducation;
 
+use App\Models\Auth\Instructor;
 use App\Models\Auth\User;
 use App\Models\GeneralEducation\Comment;
 use App\Models\GeneralEducation\Course;
 use App\Models\GeneralEducation\Entry;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\DB;
 
 class CoursePolicy
 {
@@ -32,7 +34,20 @@ class CoursePolicy
     }
 
     public function update(User $user, Course $course){
-        return true;
+        $instructor = Instructor::where('user_id',$user->id)->first();
+        if($instructor != null){
+            $geCourseInstructor = DB::table("ge_courses_instructors")->where('course_id',$course->id)
+            ->where('instructor_id',$instructor->id)->first();
+            if($geCourseInstructor != null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
     }
 
     public function delete(User $user, Course $course){
