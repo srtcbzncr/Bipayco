50fdd4ee27cbf25e1f34e8c10310a4f5ba937df4
SergenKoca <sergenkoca@outlook.com.tr>
2020-01-24 13:35:31 +0300

    Update LessonRepository.php

--- app/Repositories/GeneralEducation/LessonRepository.php
+++ app/Repositories/GeneralEducation/LessonRepository.php
@@ -64,8 +64,8 @@ class LessonRepository implements IRepository{
         try{
             DB::beginTransaction();
             $fileExtension = $data['document']->extension();
-            ini_set('upload_max_filesize', '10M');
-            ini_set('post_max_size', '10M');
+            ini_set('upload_max_filesize', '500M');
+            ini_set('post_max_size', '500M');
             ini_set('max_input_time', 300);
             ini_set('max_execution_time', 300);
             $filePath = Storage::putFile('lessons', $data['document']);
