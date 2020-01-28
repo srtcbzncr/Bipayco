e3f1ff431c442dca6d52952fb86a2176a1a77f23
Bedri Furkan Aydar <bedrifa@outlook.com>
2020-01-28 12:37:42 +0300

    Update course-section.vue

--- resources/js/components/instructor/course-section.vue
+++ resources/js/components/instructor/course-section.vue
@@ -183,26 +183,30 @@
             },
             addLesson:function () {
                 var isPreview = document.querySelector('#'+this.preview).checked ? 1 : 0;
-                var doc;
+                let doc;
                 if(this.isVideo=='1'){
                     doc=document.querySelector('#'+this.courseVideo).files[0];
                 }else{
                     doc=document.querySelector('#'+this.coursePdf).files[0];
                 }
-                var source=document.querySelector('#'+this.courseSource);
+                let source=document.querySelector('#'+this.courseSource);
                 var courseSources=[];
                 for (var i=0; i<source.files.length;i++){
                     courseSources.push(source.files[i]);
-                    console.log(source.files[i]);
                 }
                 var formData=new FormData();
                 formData.append('name', document.getElementById(this.lessonInput).value);
                 formData.append('is_preview', isPreview);
                 formData.append('is_video', this.isVideo);
                 formData.append('document', doc);
-                formData.append('source',courseSources);
+                for(var i=0;i<courseSources.length;i++){
+                    formData.append('source['+i+']',courseSources[i]);
+                }
                 formData.append('courseId', this.courseId);
-
+                for(var pair of formData.entries()){
+                    console.log(pair[0]);
+                    console.log(pair[1]);
+                }
                 axios.post('/api/instructor/course/'+this.courseId+'/sections/'+this.section.id+'/lessons/create', formData, {
                     headers: {
                         'Content-Type': 'multipart/form-data'
