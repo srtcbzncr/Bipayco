a6faee6668da215919f9d6a7bc74b1304ed1a442
SergenKoca <sergenkoca@outlook.com.tr>
2020-02-05 15:54:43 +0300

    Update CourseRepository.php

--- app/Repositories/GeneralEducation/CourseRepository.php
+++ app/Repositories/GeneralEducation/CourseRepository.php
@@ -872,7 +872,6 @@ class CourseRepository implements IRepository{
         }
 
         try{
-            $course = null;
             DB::beginTransaction();
             $course = Course::find($course_id);
             DB::table("ge_courses_instructors")->where('course_id',$course_id)->delete();
