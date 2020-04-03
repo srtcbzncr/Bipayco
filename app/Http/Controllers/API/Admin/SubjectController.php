1c7994ee240c1d42666141564354a2796417ef7e
SergenKoca <sergenkoca@outlook.com.tr>
2020-04-03 18:06:37 +0300

    CRUD cirrculum subject

--- app/Http/Controllers/API/Admin/SubjectController.php
+++ app/Http/Controllers/API/Admin/SubjectController.php
@@ -3,27 +3,115 @@
 namespace App\Http\Controllers\API\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Admin\SubjectRepository;
 use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
     public function createSubject(Request $request){
+        // initializing
+        $data = $request->toArray();
+        $repo = new SubjectRepository();
 
+        // operations
+        $resp = $repo->create($data);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Konu başarıyla oluşturuldu.',
+                'data' => $resp->getData()
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Konu oluştururken bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
     }
 
-    public function showSubjects(Request $request){
+    public function showSubjects(){
+        // initializing
+        $repo = new SubjectRepository();
 
+        // operations
+        $resp = $repo->all();
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Konular başarıyla getirildi.',
+                'data' => $resp->getData()
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Konular getirilirken bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
     }
 
-    public function showSubject(Request $request){
+    public function showSubject($subjectId){
+        // initializing
+        $repo = new SubjectRepository();
 
+        // operations
+        $resp = $repo->get($subjectId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Konu başarıyla getirildi.',
+                'data' => $resp->getData()
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Konu getirilirken bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
     }
 
-    public function deleteSubject(Request $request){
+    public function deleteSubject($subjectId){
+        // initializing
+        $repo = new SubjectRepository();
 
+        // operations
+        $resp = $repo->delete($subjectId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Konu başarıyla silindi.',
+                'data' => $resp->getData()
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Konu silinirken bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
     }
 
-    public function updateSubject(Request $request){
+    public function updateSubject($subjectId,Request $request){
+        // initializing
+        $data = $request->toArray();
+        $repo = new SubjectRepository();
 
+        // operations
+        $resp = $repo->update($subjectId,$data);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Konu başarıyla güncellendi.',
+                'data' => $resp->getData()
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Konu güncellenirken bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
     }
 }
--- app/Repositories/Admin/SubjectRepository.php
+++ app/Repositories/Admin/SubjectRepository.php
@@ -4,7 +4,144 @@
 namespace App\Repositories\Admin;
 
 
-class SubjectRepository
+use App\Models\Curriculum\Subject;
+use App\Repositories\IRepository;
+use App\Repositories\RepositoryResponse;
+use Illuminate\Support\Facades\DB;
+
+class SubjectRepository implements IRepository
 {
 
+    public function all()
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            $object = Subject::all();
+        }
+        catch(\Exception $e){
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function get($id)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            $object = Subject::find($id);
+        }
+        catch(\Exception $e){
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function create(array $data)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            DB::beginTransaction();
+            $object = new Subject();
+            $object->lesson_id = $data['lessonId'];
+            $object->name = $data['name'];
+            $object->symbol = $data['symbol'];
+            $object->save();
+            DB::commit();
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function update($id, array $data)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            DB::beginTransaction();
+            $object = Subject::find($id);
+            $object->lesson_id = $data['lessonId'];
+            $object->name = $data['name'];
+            $object->symbol = $data['symbol'];
+            $object->save();
+            DB::commit();
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function delete($id)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            DB::beginTransaction();
+            $object = Subject::find($id);
+            $object->delete();
+            DB::commit();
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function setActive($id)
+    {
+        // TODO: Implement setActive() method.
+    }
+
+    public function setPassive($id)
+    {
+        // TODO: Implement setPassive() method.
+    }
 }
