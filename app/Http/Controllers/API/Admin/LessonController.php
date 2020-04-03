e32a85fa1badfb0b6ff10abb29a16efa35710b3f
SergenKoca <sergenkoca@outlook.com.tr>
2020-04-03 17:30:27 +0300

    CRUD cirrculum lesson

--- app/Http/Controllers/API/Admin/LessonController.php
+++ app/Http/Controllers/API/Admin/LessonController.php
@@ -3,9 +3,110 @@
 namespace App\Http\Controllers\API\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Admin\LessonRepository;
 use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
-    //
+    public function createLesson(Request $request){
+        // initializing
+        $repo = new LessonRepository();
+        $data = $request->toArray();
+
+        // operations
+        $resp = $repo->create($data);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Ders başarıyla oluşturuldu.',
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Ders oluşturulurken hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
+
+    public function showLessons(){
+        // initializing
+        $repo = new LessonRepository();
+
+        // operations
+        $resp = $repo->all();
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Dersler başarıyla getirildi.',
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Dersler getirilirken hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
+
+    public function showLesson($lessonId){
+        // initializing
+        $repo = new LessonRepository();
+
+        // operations
+        $resp = $repo->get($lessonId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Ders başarıyla getirildi.',
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Ders getirilirken hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
+
+    public function deleteLesson($lessonId){
+        // initializing
+        $repo = new LessonRepository();
+
+        // operations
+        $resp = $repo->delete($lessonId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Ders başarıyla getirildi.',
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Ders getirilirken hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
+
+    public function updateLesson($lessonId,Request $request){
+        // initializing
+        $repo = new LessonRepository();
+        $data = $request->toArray();
+
+        // operations
+        $resp = $repo->update($lessonId,$data);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'message' => 'Ders başarıyla güncellendi.',
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Ders güncellenirken hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
 }
---
+++ app/Repositories/Admin/LessonRepository.php
@@ -0,0 +1,148 @@
+<?php
+
+
+namespace App\Repositories\Admin;
+
+
+use App\Models\Curriculum\Lesson;
+use App\Models\Curriculum\Subject;
+use App\Repositories\IRepository;
+use App\Repositories\RepositoryResponse;
+use Illuminate\Support\Facades\DB;
+
+class LessonRepository implements IRepository
+{
+
+    public function all()
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            $object = Lesson::all();
+        }
+        catch(\Exception $e){
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function get($id)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            $object = Lesson::find($id);
+            $subjects = Subject::where('lesson_id',$id)->get();
+            $object['subjects'] = $subjects;
+        }
+        catch(\Exception $e){
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function create(array $data)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            DB::beginTransaction();
+            $object = new Lesson();
+            $object->name = $data['name'];
+            $object->symbol = $data['symbol'];
+            DB::commit();
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function update($id, array $data)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            DB::beginTransaction();
+            $object = new Lesson();
+            $object->name = $data['name'];
+            $object->symbol = $data['symbol'];
+            DB::commit();
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function delete($id)
+    {
+        // Response variables
+        $result = true;
+        $error = null;
+        $object = null;
+
+        // Operations
+        try{
+            $object = Lesson::find($id);
+            $subjects = Subject::where('lesson_id',$id)->get();
+            foreach ($subjects as $subject){
+                $tempSubject = Subject::find($subject->id);
+                $tempSubject->delete();
+            }
+            $object->delete();
+        }
+        catch(\Exception $e){
+            $error = $e->getMessage();
+            $result = false;
+        }
+
+        // Response
+        $resp = new RepositoryResponse($result, $object, $error);
+        return $resp;
+    }
+
+    public function setActive($id)
+    {
+        // TODO: Implement setActive() method.
+    }
+
+    public function setPassive($id)
+    {
+        // TODO: Implement setPassive() method.
+    }
+}
