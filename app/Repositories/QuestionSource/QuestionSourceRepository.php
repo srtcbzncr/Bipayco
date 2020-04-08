9a528d1dbb817769dc8eb30cc0f6384f1e1dcd44
SergenKoca <sergenkoca@outlook.com.tr>
2020-04-08 17:04:32 +0300

    Update QuestionSourceRepository.php

--- app/Repositories/QuestionSource/QuestionSourceRepository.php
+++ app/Repositories/QuestionSource/QuestionSourceRepository.php
@@ -10,6 +10,7 @@ use App\Models\Curriculum\Lesson;
 use App\Models\Curriculum\Subject;
 use App\Models\QuestionSource\GapFilling;
 use App\Models\QuestionSource\MultiChoice;
+use App\Models\QuestionSource\Order;
 use App\Models\QuestionSource\Question;
 use App\Models\QuestionSource\SingleChoice;
 use App\Models\QuestionSource\TrueFalse;
@@ -234,7 +235,6 @@ class QuestionSourceRepository implements IRepository
                 $object->level = $data['level'];
                 $object->type = 'App\Models\QuestionSource\TrueFalse';
                 $object->crLessonId = $data['crLessonId'];
-                $object->crLessonId = $data['crLessonId'];
                 $object->crSubjectId = $data['crSubjectId'];
                 $object->instructorId = $data['instructorId'];
                 $object->isConfirm = false;
@@ -247,7 +247,22 @@ class QuestionSourceRepository implements IRepository
                 $objectAnswer->save();
             }
             else if($data['type'] == 'order'){
+                $object->text = $data['text'];
+                $object->level = $data['level'];
+                $object->crLessonId = $data['crLessonId'];
+                $object->crSubjectId = $data['crSubjectId'];
+                $object->instructorId = $data['instructorId'];
+                $object->isConfirm = false;
+                $object->save();
 
+                // answers ekle
+                foreach ($data['content'] as $key=> $item){
+                    $objectAnswer = new Order();
+                    $objectAnswer->questionId = $object->id;
+                    $objectAnswer->content = $item;
+                    $objectAnswer->no = $key;
+                    $objectAnswer->save();
+                }
             }
             else if($data['type'] == 'match'){
 
