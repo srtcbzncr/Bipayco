e8cced21162ef429bb7005a9f9643260d4b53e7d
SergenKoca <sergenkoca@outlook.com.tr>
2020-04-10 22:04:17 +0300

    Update QuestionSourceRepository.php

--- app/Repositories/QuestionSource/QuestionSourceRepository.php
+++ app/Repositories/QuestionSource/QuestionSourceRepository.php
@@ -72,7 +72,7 @@ class QuestionSourceRepository implements IRepository
         $veri =str_replace("<","",$veri);
         $veri =str_replace(">","",$veri);
         $veri =str_replace("*","",$veri);
-        $veri =str_replace("And","",$veri);
+        $veri =str_replace(":","",$veri);
         $veri =str_replace("'","",$veri);
         $veri =str_replace("chr(34)","",$veri);
         $veri =str_replace("chr(39)","",$veri);
