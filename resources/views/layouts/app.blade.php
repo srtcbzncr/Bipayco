186dc10a9599c57b26d7a2de2edbc33ad344fef0
Bedri Furkan Aydar <bedrifa@outlook.com>
2020-05-03 12:43:19 +0300

    added icon to tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="shortcut icon" href="{{asset('/images/Bipayco.ico')}}">
     <title> Bipayco </title>
 
     <!-- Scripts -->
