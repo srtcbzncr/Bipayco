f672900496f5080dfeca1e86ef8616eb4fd80828
SergenKoca <sergenkoca@outlook.com.tr>
2020-05-22 16:28:00 +0300

    Update NotificationController.php

--- app/Http/Controllers/API/Notification/NotificationController.php
+++ app/Http/Controllers/API/Notification/NotificationController.php
@@ -3,11 +3,80 @@
 namespace App\Http\Controllers\API\Notification;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\UserOperations\NotificationRepository;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
     public function show($userId){
+        $repo = new NotificationRepository();
+        $resp = $repo->show($userId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'data' => $resp->getData(),
+                'message' => 'Bildirimler başarıyla getirildi.'
+            ]);
+        }
 
+        return response()->json([
+           'error' => true,
+           'message' => 'Bildirimler getirilirken bir hata meydana geldi.Tekrar deneyin.',
+           'errorMessage' => $resp->getError()
+        ]);
+    }
+
+    public function get($notificationId){
+        $repo = new NotificationRepository();
+        $resp = $repo->get($notificationId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'data' => $resp->getData(),
+                'message' => 'Bildirim başarıyla getirildi.'
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Bildirim getirilirken bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ]);
+    }
+
+    public function delete($notificationId){
+        $repo = new NotificationRepository();
+        $resp = $repo->delete($notificationId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'data' => $resp->getData(),
+                'message' => 'Bildirim başarıyla silindi.'
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Bildirim silindi bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ]);
+    }
+
+    public function seen($notificationId){
+        $repo = new NotificationRepository();
+        $resp = $repo->seen($notificationId);
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'data' => $resp->getData(),
+                'message' => 'Bildirimin görüntülenme durumu başarıyla güncellendi.'
+            ]);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message' => 'Bildirimin görüntülenme durumu güncellenirken bir hata meydana geldi.Tekrar deneyin.',
+            'errorMessage' => $resp->getError()
+        ]);
     }
 }
