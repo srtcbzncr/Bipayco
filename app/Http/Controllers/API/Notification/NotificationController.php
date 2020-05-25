4d00104c5cdbe7a05efe974d888f4a3ecf163eac
SergenKoca <sergenkoca@outlook.com.tr>
2020-05-25 14:55:58 +0300

    update

--- app/Http/Controllers/API/Notification/NotificationController.php
+++ app/Http/Controllers/API/Notification/NotificationController.php
@@ -79,4 +79,52 @@ class NotificationController extends Controller
             'errorMessage' => $resp->getError()
         ]);
     }
+
+    public function accept(){
+        $repo = new NotificationRepository();
+        $resp = $repo->accept();
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Bir hata meydana geldi.Tekrar deneyin',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
+
+    public function reject(){
+        $repo = new NotificationRepository();
+        $resp = $repo->reject();
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Bir hata meydana geldi.Tekrar deneyin',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
+
+    public function redirect(){
+        $repo = new NotificationRepository();
+        $resp = $repo->redirect();
+        if($resp->getResult()){
+            return response()->json([
+                'error' => false,
+                'data' => $resp->getData()
+            ]);
+        }
+        return response()->json([
+            'error' => true,
+            'message' => 'Bir hata meydana geldi.Tekrar deneyin',
+            'errorMessage' => $resp->getError()
+        ],400);
+    }
 }
