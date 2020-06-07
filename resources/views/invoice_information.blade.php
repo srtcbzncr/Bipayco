b273de675b1ec77854b99dc6d77e2ede63991fa7
Bedri Furkan Aydar <bedrifa@outlook.com>
2020-06-08 00:33:54 +0300

    Update invoice_information.blade.php

--- resources/views/invoice_information.blade.php
+++ resources/views/invoice_information.blade.php
@@ -52,6 +52,7 @@
                                 @endif
                                 <div class="uk-child-width-1-2@l uk-grid">
                                     <div class="uk-margin-small-bottom">
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.first_name')</div>
                                         <input id="first_name" class="uk-input form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus placeholder=" @lang('front/auth.first_name') " type="text">
                                         @error('first_name')
                                         <span class="invalid-feedback" role="alert">
@@ -60,6 +61,7 @@
                                         @enderror
                                     </div>
                                     <div>
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.last_name')</div>
                                         <input id="last_name" class="uk-input form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus placeholder=" @lang('front/auth.last_name') " type="text">
                                         @error('last_name')
                                         <span class="invalid-feedback" role="alert">
@@ -70,6 +72,7 @@
                                 </div>
                                 <div class="uk-child-width-1-2@l uk-grid uk-margin-small">
                                     <div class="uk-margin-small-bottom">
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.email')</div>
                                         <input id="email" class="uk-input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="@lang('front/auth.email')" type="text">
                                         @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -78,6 +81,7 @@
                                         @enderror
                                     </div>
                                     <div>
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.phone_number')</div>
                                         <input id="phone_number" class="uk-input form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number') }}" required autocomplete="phone_number" autofocus placeholder="@lang('front/auth.phone_number')" type="text">
                                         @error('phone_number')
                                         <span class="invalid-feedback" role="alert">
@@ -88,6 +92,7 @@
                                 </div>
                                 <div class="uk-child-width-1-2@l uk-grid uk-margin-small">
                                     <div class="uk-margin-small-bottom">
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.identity_number')</div>
                                         <input id="identity_number" class="uk-input form-control @error('identity_number') is-invalid @enderror" name="identity_number" value="{{ old('identity_number') }}" required autocomplete="identity_number" placeholder="@lang('front/auth.identity_number')" type="text">
                                         @error('identity_number')
                                         <span class="invalid-feedback" role="alert">
@@ -96,6 +101,7 @@
                                         @enderror
                                     </div>
                                     <div>
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.country')</div>
                                         <input id="country" class="uk-input form-control @error('country') is-invalid @enderror" name="country" value="{{ old('country') }}" required autocomplete="country" autofocus placeholder="@lang('front/auth.country')" type="text">
                                         @error('country')
                                         <span class="invalid-feedback" role="alert">
@@ -104,18 +110,18 @@
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="uk-grid uk-margin-small">
-                                    <div class="uk-margin-small-bottom">
-                                        <textarea style="resize:none;" class="uk-textarea uk-width uk-height-small uk-overflow form-control @error('address') is-invalid @enderror" name="address" id="address" autocomplete="address" placeholder="@lang('front/auth.address')" required>{{ old('address') }}</textarea>
-                                        @error('address')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
+                                <div class="uk-margin-small">
+                                    <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.address')</div>
+                                    <textarea style="resize:none; height: 150px" class="uk-textarea uk-width uk-overflow form-control @error('address') is-invalid @enderror" name="address" id="address" autocomplete="address" placeholder="@lang('front/auth.address')" required>{{ old('address') }}</textarea>
+                                    @error('address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
                                 </div>
                                 <div class="uk-child-width-1-2@l uk-grid uk-margin-small">
-                                    <div class="uk-margin-small-bottom uk-inline uk-flex align-items-center">
+                                    <div class="uk-margin-small-bottom">
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.city')</div>
                                         <input id="city" class="uk-input form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city" autofocus placeholder="@lang('front/auth.city')" type="text">
                                         @error('city')
                                         <span class="invalid-feedback" role="alert">
@@ -124,6 +130,7 @@
                                         @enderror
                                     </div>
                                     <div>
+                                        <div class="uk-form-label uk-margin-remove-top"> @lang('front/auth.zip_code')</div>
                                         <input id="zip_code" class="uk-input form-control @error('zip_code') is-invalid @enderror" name="zip_code" value="{{ old('zip_code') }}" required autocomplete="zip_code" autofocus placeholder="@lang('front/auth.zip_code')" type="text">
                                         @error('zip_code')
                                         <span class="invalid-feedback" role="alert">
