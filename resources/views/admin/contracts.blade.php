7eed614229f3054eeb14298ebae1bb3001a01249
Bedri Furkan Aydar <bedrifa@outlook.com>
2020-07-11 17:11:37 +0300

    Update contracts.blade.php

--- resources/views/admin/contracts.blade.php
+++ resources/views/admin/contracts.blade.php
@@ -2,14 +2,15 @@
 @section('content')
     <div class="uk-margin-medium-top">
         <div class="uk-background-default uk-padding-remove uk-margin-small-top border-radius-6">
-            <h2>@lang('front/auth.cookies_policy')</h2>
+            <h3>@lang('front/auth.cookies_policy')</h3>
             <form method="POST" action="{{route('admin_contract_post')}}">
-                <div class="uk-flex align-items-center ">
-                    <div uk-form-custom="target: true" class="uk-flex uk-flex-center uk-width">
+                @csrf
+                <div class="uk-flex uk-flex-wrap align-items-center ">
+                    <div uk-form-custom="target: true" class="uk-flex uk-flex-center uk-width-1-2@m">
                         <input id="cookies" name="cookies_policy" type="file" accept="application/pdf,application/vnd.ms-excel" required>
                         <input class="uk-input" type="text" tabindex="-1" placeholder="@lang('front/auth.select_file')" disabled>
                     </div>
-                    <button class="uk-button uk-button-secondary">@lang('front/auth.save')</button>
+                    <button class="uk-button uk-button-secondary uk-width-1-6@m">@lang('front/auth.save')</button>
                 </div>
             </form>
         </div>
